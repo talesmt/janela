8c8f5916df60cb71609a776ee27e6880404103bf
lucassm <lucas.silveiramelo@gmail.com>
2015-04-24 21:26:52 -0300

    Inclusão do arquivo janela.py no código

---
+++ janela.py
@@ -0,0 +1,28 @@
+import sys
+from PySide import QtGui
+
+class Example(QtGui.QWidget):
+
+    def __init__(self):
+        super(Example, self).__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        label = QtGui.QLabel('Hello World 3!', self)
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Janela')
+        self.show()
+
+
+def main():
+
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
